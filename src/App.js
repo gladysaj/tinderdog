febe534adac3ab8641817d7f4d47df1bb00c2b1a
Ben Balderas <benbalderas3@gmail.com>
2020-05-20 21:19:41 -0500

    adjusted theme. adjusted dog card

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import DogCard from './components/DogCard';
 function App() {
   return (
     <div className="App">
-      <DogCard image="https://source.unsplash.com/Nu_UiANKSa8/1600x900" name="Moira" breed="German Shepherd" description="Moira is a sweet, loyal dog. She understands basic commands and loves being around people." />
+      <DogCard image="https://source.unsplash.com/z26TAz2FuuE" name="Moira" breed="German Shepherd" description="Moira is a sweet, loyal dog. She understands basic commands and loves being around people." />
     </div>
   );
 }
--- src/components/DogCard/index.js
+++ src/components/DogCard/index.js
@@ -4,22 +4,19 @@ import React from "react";
 const Card = ({ image, name, breed, description }) => {
   return (
     <div
-      className="uk-child-width-1-3@m uk-flex-center uk-padding-large uk-text-left"
+      className="uk-child-width-1-3@m uk-child-width-1-2@s uk-flex-center uk-padding-large uk-text-left"
       uk-grid="true"
     >
       <div>
-        <div className="uk-card uk-card-default">
-          <div className="uk-card-media-top uk-cover-container">
-            <img src={image} alt={name} />
-          </div>
-
-          <div className="uk-card-body">
+        <div className="uk-card uk-card-default uk-cover-container">
+          <div className="uk-card-body uk-light uk-margin-xxlarge-top uk-overlay">
             <h2 className="uk-text-bold uk-text-left uk-text-xlarge">{name}</h2>
-            <p class="uk-pill uk-text-left uk-text-small uk-text-bold uk-text-uppercase uk-light uk-background-secondary">
+            <p class="uk-pill uk-text-left uk-text-small uk-text-bold uk-text-uppercase uk-background-primary">
               {breed}
             </p>
-            <p className="uk-text-left">{description}</p>
+            <p className="uk-text-left uk-text-emphasis">{description}</p>
           </div>
+          <img src={image} alt={name} uk-cover="true" className="bg-img" />
         </div>
       </div>
     </div>
