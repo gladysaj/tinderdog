d9f83d5e7a6a0d1d7a8ff2979c77be0d0cca3b57
Ben Balderas <benbalderas3@gmail.com>
2020-05-21 21:26:23 -0500

    added buttons. added foster and match cards

--- src/components/DogCard/index.js
+++ src/components/DogCard/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 // this is destructuring the props object e.g. props.image
-const Card = ({ image, name, breed, description }) => {
+const Card = ({ image, name, breed, age, description }) => {
   return (
     <div
       className="uk-child-width-1-3@m uk-child-width-1-2@s uk-flex-center uk-padding-large uk-text-left"
@@ -11,9 +11,16 @@ const Card = ({ image, name, breed, description }) => {
         <div className="uk-card uk-card-default uk-cover-container">
           <div className="uk-card-body uk-light uk-margin-xxlarge-top uk-overlay">
             <h2 className="uk-text-bold uk-text-left uk-text-xlarge">{name}</h2>
-            <p class="uk-pill uk-text-left uk-text-small uk-text-bold uk-text-uppercase uk-background-primary">
-              {breed}
-            </p>
+            <div className="uk-flex">
+              <p class="uk-pill uk-text-left uk-text-small uk-text-bold uk-text-uppercase uk-background-primary">
+                {breed}
+              </p>
+
+              <p class="uk-pill uk-text-left uk-text-small uk-text-bold uk-text-uppercase uk-background-primary">
+                {age}
+              </p>
+            </div>
+
             <p className="uk-text-left uk-text-emphasis">{description}</p>
           </div>
           <img src={image} alt={name} uk-cover="true" className="bg-img" />
