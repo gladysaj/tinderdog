2c2bcd6bb17cc0cd2de1f46ab54ae6e9c2e606ac
FanyMarin <nicte-ha.marin@hotmail.com>
2020-05-22 18:00:41 -0500

    Routes.js controller added, AuthForm added (first commit)

---
+++ src/components/AuthForm/index.js
@@ -0,0 +1,80 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class AuthForm extends Component {
+  state = {
+    user: {},
+  };
+
+  handleChange = () => {};
+  handleSubmit = () => {};
+
+  render() {
+    const isLogin = this.props.location.pathname === "/login";
+    return (
+      <section className="uk-section">
+        <div className="uk-container uk-flex uk-flex-center">
+          <div className="uk-width-1-4">
+            <h3>{isLogin ? "Login" : "Signup"}</h3>
+            <form
+              onSubmit={this.handleSubmit}
+              className="uk-width-1-1 uk-form-stacked uk-flex uk-flex-center uk-flex-column"
+            >
+              <div className="uk-margin">
+                <label className="uk-form-label" htmlFor="email">
+                  Email:
+                </label>
+                <div className="uk-inline">
+                  <span
+                    className="uk-form-icon uk-form-icon-flip"
+                    uk-icon="icon: mail"
+                  ></span>
+                  <input
+                    onChange={this.handleChange}
+                    id="email"
+                    name="email"
+                    className="uk-input"
+                    type="email"
+                    required
+                  />
+                </div>
+                <div className="uk-margin">
+                  <label className="uk-form-label" htmlFor="password">
+                    Password:
+                  </label>
+                  <div className="uk-inline">
+                    <span
+                      className="uk-form-icon uk-form-icon-flip"
+                      uk-icon="icon: lock"
+                    ></span>
+                    <input
+                      onChange={this.handleChange}
+                      id="password"
+                      name="password"
+                      className="uk-input"
+                      type="password"
+                      required
+                    />
+                  </div>
+                </div>
+              </div>
+              {isLogin ? (
+                <div className="uk-text-meta">
+                  Aún no tienes cuenta?
+                  <Link className="uk-text-primary" to="/signup">
+                    Crear cuenta
+                  </Link>
+                </div>
+              ) : null}
+              <button className="uk-button uk-button-primary">
+                {isLogin ? "Login" : "Signup"}
+              </button>
+            </form>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default AuthForm;
