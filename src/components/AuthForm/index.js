919b7a8d278ca8bd064eb1dc95052a8e0cedcf4b
FanyMarin <nicte-ha.marin@hotmail.com>
2020-05-22 22:32:25 -0500

    axios installed, AuthServices.js created, login/signup (second commit)

--- src/components/AuthForm/index.js
+++ src/components/AuthForm/index.js
@@ -1,14 +1,8 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import Login from "./Login";
+import Signup from "./Signup";
 
 class AuthForm extends Component {
-  state = {
-    user: {},
-  };
-
-  handleChange = () => {};
-  handleSubmit = () => {};
-
   render() {
     const isLogin = this.props.location.pathname === "/login";
     return (
@@ -16,60 +10,7 @@ class AuthForm extends Component {
         <div className="uk-container uk-flex uk-flex-center">
           <div className="uk-width-1-4">
             <h3>{isLogin ? "Login" : "Signup"}</h3>
-            <form
-              onSubmit={this.handleSubmit}
-              className="uk-width-1-1 uk-form-stacked uk-flex uk-flex-center uk-flex-column"
-            >
-              <div className="uk-margin">
-                <label className="uk-form-label" htmlFor="email">
-                  Email:
-                </label>
-                <div className="uk-inline">
-                  <span
-                    className="uk-form-icon uk-form-icon-flip"
-                    uk-icon="icon: mail"
-                  ></span>
-                  <input
-                    onChange={this.handleChange}
-                    id="email"
-                    name="email"
-                    className="uk-input"
-                    type="email"
-                    required
-                  />
-                </div>
-                <div className="uk-margin">
-                  <label className="uk-form-label" htmlFor="password">
-                    Password:
-                  </label>
-                  <div className="uk-inline">
-                    <span
-                      className="uk-form-icon uk-form-icon-flip"
-                      uk-icon="icon: lock"
-                    ></span>
-                    <input
-                      onChange={this.handleChange}
-                      id="password"
-                      name="password"
-                      className="uk-input"
-                      type="password"
-                      required
-                    />
-                  </div>
-                </div>
-              </div>
-              {isLogin ? (
-                <div className="uk-text-meta">
-                  Aún no tienes cuenta?
-                  <Link className="uk-text-primary" to="/signup">
-                    Crear cuenta
-                  </Link>
-                </div>
-              ) : null}
-              <button className="uk-button uk-button-primary">
-                {isLogin ? "Login" : "Signup"}
-              </button>
-            </form>
+            <div>{isLogin ? <Login /> : <Signup />}</div>
           </div>
         </div>
       </section>
