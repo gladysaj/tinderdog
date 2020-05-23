951fadf2ac9f59ead1adaca56d6568c904fe666c
Gladys Alvarez <glad.alvarez1@gmail.com>
2020-05-22 23:09:45 -0500

    Configure Navbar links

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => (
   <nav className="uk-navbar-container" uk-navbar="true">
     <div className="uk-navbar-left">
         <ul className="uk-navbar-nav">
-            <li className="uk-active"><a href="/">TinderDog</a></li>
+            <li className="uk-active"><Link to="/">TinderDog</Link></li>
         </ul>
     </div>
     <div className="uk-navbar-right">
         <ul className="uk-navbar-nav">
-            <li><a href="#">Register</a></li>
-            <li><a href="#">Login</a></li>
+            <li><Link to="/signup">Register</Link></li>
+            <li><Link to="/login">Login</Link></li>
         </ul>
     </div>
 </nav>
