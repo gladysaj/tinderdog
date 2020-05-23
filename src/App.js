6541d7297829a5c608e13c516fd54c119b3cc818
Ben Balderas <benbalderas3@gmail.com>
2020-05-23 01:13:52 -0500

    added app context

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from "react";
-import "./App.css";
-import Routes from "./Routes";
-import NavBar from './components/NavBar';
-// import AppContext from '../AppContext'
+import React, { Component } from 'react';
+import './App.css';
+import Navbar from './components/NavBar';
+import Routes from './Routes';
+import AppContext from './AppContext'
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar/>
-      <Routes />
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    user: JSON.parse(localStorage.getItem("user")) || {}
+  }
+
+  setUser = (user) => {
+    console.log("set user", user)
+    this.setState({ user });
+  }
+
+  render() {
+    const { state, setUser } = this;
+
+    return (
+      // wrap the app in the context provider so the whole app has access to the data
+      <AppContext.Provider value={{ state: state, setUser }}>
+        <div className="App">
+          <Navbar />
+          <Routes />
+        </div>
+      </AppContext.Provider>
+    );
+  }
 }
 
 export default App;
--- src/components/AuthForm/index.js
+++ src/components/AuthForm/index.js
@@ -1,21 +1,141 @@
 import React, { Component } from "react";
-import Login from "./Login";
-import Signup from "./Signup";
+import { login, signup } from "../../services/authServices";
+import { Link } from "react-router-dom";
+import AppContext from "../../AppContext";
+import UIKit from "uikit";
 
 class AuthForm extends Component {
-  render() {
+  state = {
+    data: {},
+    showPassword: false,
+  };
+
+  handleShowPassword = () => {
+    const { showPassword } = this.state;
+    const showValue = !showPassword;
+    this.setState({ showPassword: showValue });
+  };
+
+  handleChange = (e) => {
+    let { data } = this.state;
+    // spread operator allows for persistend data -> adding the key and value to the same object
+    // "name" is the attribute in the form field, "value" is whatever value the field contains at the time
+    data = { ...data, [e.target.name]: e.target.value };
+    this.setState({ data });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { data } = this.state;
+    const { setUser } = this.context;
+    const { history } = this.props;
     const isLogin = this.props.location.pathname === "/login";
+
+    // return the service depending on the pathname
+    const action = isLogin ? login : signup;
+    const nextRoute = isLogin ? "/" : "login";
+
+    // convert credentials to string and save in local storage
+    action(data)
+      .then((res) => {
+        const { user } = res.data;
+        localStorage.setItem("user", JSON.stringify(user));
+        setUser(user);
+
+        history.push(nextRoute);
+      })
+      .catch((err) => {
+        // Handle errors on front-end with UIKit
+        UIKit.notification({
+          message: `<span uk-icon='icon: close'></span> ${err.response.data.msg}`,
+          status: "warning",
+          pos: "top-right",
+        });
+      });
+  };
+
+  render() {
+    // Compare with the props returned by react-router-dom
+    const isLogin = this.props.location.pathname === "/login";
+
     return (
       <section className="uk-section">
         <div className="uk-container uk-flex uk-flex-center">
           <div className="uk-width-1-4">
-            <h3>{isLogin ? "Login" : "Signup"}</h3>
-            <div>{isLogin ? <Login /> : <Signup />}</div>
+            <h1>{isLogin ? "Login" : "Signup"}</h1>
+
+            <form
+              onSubmit={this.handleSubmit}
+              className="uk-width-1-1 uk-form-stacked uk-flex uk-flex-center uk-flex-column"
+            >
+              <div className="uk-margin">
+                <label className="uk-form-label" htmlFor="email">
+                  Email:
+                </label>
+                <div className="uk-inline">
+                  <span
+                    className="uk-form-icon uk-form-icon-flip"
+                    uk-icon="icon: mail"
+                  ></span>
+                  <input
+                    onChange={this.handleChange}
+                    id="email"
+                    name="email"
+                    className="uk-input"
+                    type="text"
+                    required
+                  />
+                </div>
+
+                <div>
+                  <label className="uk-form-label" htmlFor="password">
+                    Password:
+                  </label>
+                  <div className="uk-inline">
+                    <span
+                      className="uk-form-icon uk-form-icon-flip"
+                      uk-icon="icon: lock"
+                    ></span>
+                    <input
+                      onChange={this.handleChange}
+                      id="password"
+                      name="password"
+                      className="uk-input"
+                      type={this.state.showPassword ? "text" : "password"}
+                      required
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <p
+                className="uk-text-meta uk-text-primary cursor-pointer"
+                onClick={this.handleShowPassword}
+              >
+                Show password
+              </p>
+
+              <button className="uk-button uk-button-primary">
+                {isLogin ? "Login" : "Signup"}
+              </button>
+
+              {isLogin ? (
+                <div className="uk-text-meta uk-margin-top">
+                  Not a member yet?{" "}
+                  <Link to="/signup" className="uk-text-primary">
+                    Signup here
+                  </Link>
+                </div>
+              ) : null}
+            </form>
           </div>
         </div>
       </section>
     );
   }
 }
+
+AuthForm.contextType = AppContext;
 
 export default AuthForm;
