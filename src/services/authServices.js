fed05d5cff9201c85891d27d628323f605d50cbe
Ben Balderas <benbalderas3@gmail.com>
2020-05-29 15:31:02 -0500

    added base url variable

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,18 +1,19 @@
 import axios from "axios";
+import { base_url } from "./variables";
 
 axios.defaults.withCredentials = true;
 
 export const login = (credential) => {
-  return axios.post("http://localhost:3000/api/login", credential);
+  return axios.post(`${base_url}/login`, credential);
 };
 
 //Esta ruta no estaba sirviendo porque para poder hacer un signup en el modelo de usuario estaba
 //requerido el numero de telefono y el nombre. Para arreglarlo, le agregue el campo de Phone number y
 //name en el proceso del signup
 export const signup = (credential) => {
-  return axios.post("http://localhost:3000/api/signup", credential);
+  return axios.post(`${base_url}/signup`, credential);
 };
 
 export const logout = () => {
-  return axios.post("http://localhost:3000/api/logout");
+  return axios.post(`${base_url}/logout`);
 };
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import { base_url } from "./variables";
 
 // needed to send credentials to cookie so login is not lost with url change
 axios.defaults.withCredentials = true;
 
 export const getOwnerDogs = () => {
-    return axios.get("http://localhost:3000/api/find-dog")
+    return axios.get(`${base_url}/find-dog`)
 }
